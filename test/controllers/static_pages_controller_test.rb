b8bc32c176b2b99109b61164a145d24d1dba7e9c
Emma Beaton <emma@reax.io>
2016-04-19 22:49:08 -0500

    updated title view variable with application helper function

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Twitter Ripoff"
+    assert_select "title", "Twitter Ripoff"
   end
 
   test "should get help" do
