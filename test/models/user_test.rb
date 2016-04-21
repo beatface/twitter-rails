a7a16ff1ed7aa6bfaa355c15486bd1d76313ba73
Emma Beaton <emma@reax.io>
2016-04-21 18:39:26 -0500

    added user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Foo Bar', email: 'foo@bar.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'email should be less than 50 chars' do
+    assert @user.email.length < 50
+  end
 end
