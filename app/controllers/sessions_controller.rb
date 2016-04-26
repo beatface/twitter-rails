9ea086fade9d79efbfd145ec1c963f45bae5cd2b
Emma Beaton <emma@reax.io>
2016-04-26 11:24:41 -0500

    added flash message and corresponding test for invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log user in and redirect to profile page
+    else
+      # show an error
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 
   def destroy
